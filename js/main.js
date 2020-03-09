343e23160360f4ea1443212ee749bfdf267f1850
Alex Attemann <a.attemann@gmail.com>
2020-03-09 21:59:50 +0000

    Added brackets & rounding

--- js/main.js
+++ js/main.js
@@ -5,25 +5,38 @@ const calcInput = document.querySelector("#calcInput");
 
 let inputArray = [];
 
-numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        inputArray.push(number.value);
-        calcInput.textContent = inputArray.join("");
-    })
-})
-
+numbers.forEach(number => {
+  number.addEventListener("click", () => {
+    inputArray.push(number.value);
+    calcInput.textContent = inputArray.join("");
+    console.log(inputArray);
+  });
+});
 
 function calculate(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
-    calcInput.textContent = eval(sum);
+  // let sum = 0;
+  // for (let i = 0; i < array.length; i++) {
+  //     sum += array[i];
+  // }
+  sum = eval(inputArray.join(""));
+  roundedSum = Math.round((sum + Number.EPSILON) * 100) / 100;
+  calcInput.textContent = roundedSum;
+  inputArray = [roundedSum];
 }
 
-equals.addEventListener("click", function(){calculate(inputArray)}, false);
+equals.addEventListener(
+  "click",
+  function() {
+    calculate(inputArray);
+  },
+  false
+);
 
-clear.addEventListener("click", () => {
+clear.addEventListener(
+  "click",
+  () => {
     inputArray = [];
     calcInput.textContent = "";
-}, false);
+  },
+  false
+);
