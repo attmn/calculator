b3d85a95a3b407006c28ec04634fad75ce210f1c
Alex Attemann <a.attemann@gmail.com>
2020-03-11 21:53:35 +0000

    Added calculation error, use of numbers on keyboard and backspace operation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const numbers = document.querySelectorAll(".number");
 const equals = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
 const calcInput = document.querySelector("#calcInput");
+const lastAnswer = document.querySelector("#lastAnswer");
 
 let inputArray = [];
 
@@ -14,14 +15,15 @@ numbers.forEach(number => {
 });
 
 function calculate(array) {
-  // let sum = 0;
-  // for (let i = 0; i < array.length; i++) {
-  //     sum += array[i];
-  // }
-  sum = eval(inputArray.join(""));
-  roundedSum = Math.round((sum + Number.EPSILON) * 100) / 100;
-  calcInput.textContent = roundedSum;
-  inputArray = [roundedSum];
+  try {
+    sum = eval(inputArray.join(""));
+    roundedSum = Math.round((sum + Number.EPSILON) * 100) / 100;
+    calcInput.textContent = roundedSum;
+    inputArray = [roundedSum];
+  } catch (err) {
+    calcInput.textContent = "CALCULATION ERROR";
+    nputArray = [];
+  }
 }
 
 equals.addEventListener(
@@ -37,6 +39,40 @@ clear.addEventListener(
   () => {
     inputArray = [];
     calcInput.textContent = "";
+  },
+  false
+);
+
+//Use backspace to delete entered characters
+document.addEventListener(
+  "keydown",
+  function(e) {
+    if (e.code === "Backspace") {
+      inputArray.splice(-1, 1);
+      calcInput.textContent = inputArray.join("");
+      console.log(inputArray);
+    }
+  },
+  false
+);
+
+//Use numbers & operators on keyboard
+document.addEventListener(
+  "keydown",
+  function(e) {
+    if (parseInt(e.key) >= 0 && parseInt(e.key) <= 9) {
+      inputArray.push(e.key);
+      calcInput.textContent = inputArray.join("");
+      console.log(inputArray);
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "keydown",
+  function(e) {
+    console.log(parseInt(e.key));
   },
   false
 );
